d26623798c455eba0f4cfd4e0b4ef614df7d3f99
BeyNajib <56679862+BeyNajib@users.noreply.github.com>
2022-02-18 16:43:07 +0100

    User Story n°4
    En tant qu’ingénieur système, je veux que vous me mettiez en place un système de load
    balancing du service show-weather

--- src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
+++ src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
@@ -3,10 +3,12 @@ package com.howtodoinjava.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication (exclude = { SecurityAutoConfiguration.class })
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
