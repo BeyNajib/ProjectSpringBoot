0ad8e1590dac09ddd2f02fd572abd0037342a4d2
BeyNajib <56679862+BeyNajib@users.noreply.github.com>
2022-02-18 16:35:57 +0100

    User Story n°5
    En tant qu’ingénieur système je souhaite avoir une interface de monitoring basé sur Eureka
    dans laquelle s’enregistre tout les services mis qui sont mis à disposition avant

--- src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
+++ src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
@@ -3,8 +3,10 @@ package com.howtodoinjava.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication (exclude = { SecurityAutoConfiguration.class })
+@EnableEurekaClient
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
